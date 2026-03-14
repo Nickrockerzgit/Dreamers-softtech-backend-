8b2a650e7a9ca13679b34824fbd1bd16cf5c2161
RishabhsCode <149584213+Nickrockerzgit@users.noreply.github.com>
2026-03-15 00:24:14 +0530

    Enable CORS for localhost and production origin

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ const app = express();
 // credentials: true → allows cookies to be sent cross-origin
 app.use(
   cors({
-    // origin: "http://localhost:5173",
+    origin: "http://localhost:5173",
     origin: "https://dreamers-softtech.vercel.app",
     credentials: true,
   }),
